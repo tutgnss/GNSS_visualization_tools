2bf5a6e61bb3f7d56da6783fc1c327123475b8a2
Pedro Figueiredo e Silva <PFigs@users.noreply.github.com>
2016-07-03 18:39:23 +0300

    Define main code
    Avoid mixing code with classes and separate it explicitly using the 
    
    if __name__ == "__main__"

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+# Tampere University of Technology
+#
+# DESCRIPTION
+#
+#
+# AUTHOR(S)
+# Anne-Marie Tobie
+
 __author__ = 'tobie'
 
 import connection
@@ -7,36 +15,6 @@ import data_processing
 import time
 import pyvisa
 from threading import Thread
-
-
-res = connection.check_connection()
-#if res != 'devices connected':
-#    raise ValueError('devices not connected')
-print(res)
-
-# read the scenario
-
-Date = '05-06-2016'
-Hour = '15:01:00.0'
-Start = 10
-Duration = '00:00:07'
-Interval = 10
-
-inst = pyvisa.ResourceManager().open_resource('USB0::0x14EB::0x0060::200448::INSTR')
-scenario = config_parser.read_scen()
-
-# Set scenario to Spectracom and launch it
-
-Spectracom.clear()
-Spectracom.reset()
-Spectracom.set_init(Date, Hour, float(scenario[0][0]), float(scenario[0][1]), float(scenario[0][2]), Start, Duration, Interval)
-Spectracom.launch()
-
-# wait for the Spectracom to be really launch
-
-time.sleep(80)
-
-# read data comming from both the Spectracom and the Ublox at the same time while the scenario is running
 
 
 class Acquire_data(Thread):
@@ -56,16 +34,47 @@ class Acquire_data(Thread):
                     savefile.write(info)
             savefile.close()
 
+if __name__ == "__main__":
 
-thread_1 = Acquire_data(1)
-#thread_2 = Acquire_data(2)
+    res = connection.check_connection()
+    #if res != 'devices connected':
+    #    raise ValueError('devices not connected')
+    print(res)
 
-thread_1.start()
-#thread_2.start()
+    # read the scenario
 
-thread_1.join()
-Spectracom.stop()
+    Date = '05-06-2016'
+    Hour = '15:01:00.0'
+    Start = 10
+    Duration = '00:00:07'
+    Interval = 10
 
-# computation of the root mean square error
+    inst = pyvisa.ResourceManager().open_resource('USB0::0x14EB::0x0060::200448::INSTR')
+    scenario = config_parser.read_scen()
 
-#data_processing.computation()
+    # Set scenario to Spectracom and launch it
+    
+    Spectracom.clear()
+    Spectracom.reset()
+    Spectracom.set_init(Date, Hour, float(scenario[0][0]), float(scenario[0][1]), float(scenario[0][2]), Start, Duration, Interval)
+    Spectracom.launch()
+    
+    # wait for the Spectracom to be really launch
+    
+    time.sleep(80)
+    
+    # read data comming from both the Spectracom and the Ublox at the same time while the scenario is running
+    
+    
+    thread_1 = Acquire_data(1)
+    #thread_2 = Acquire_data(2)
+    
+    thread_1.start()
+    #thread_2.start()
+    
+    thread_1.join()
+    Spectracom.stop()
+    
+    # computation of the root mean square error
+    
+    #data_processing.computation()
