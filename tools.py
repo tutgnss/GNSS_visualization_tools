30649df5b01dfd22cd80ef1d52210dc589f99b4e
A-Mari <anne-marie.tobie@tut.fi>
2016-07-29 08:50:58 +0300

    development

--- tools.py
+++ tools.py
@@ -1,25 +1,70 @@
-__author__ = 'tobie'
+# Tampere University of Technology
+#
+# DESCRIPTION
+# Defines some randoms functions
+#
+# AUTHOR
+# Anne-Marie Tobie
 
-import math, binascii
+import math
+import configparser
 
 
-class Tools:
-    def get_sec(self):
-        # convert a date in DD:HH:MM into sec
-        d, h, m = [int(i) for i in self.split(':')]
-        return d*3600*24 + h*3600 + m*60
+def get_sec(date):
+    # Converts a date into sec
+    # Input:
+    # date: date in this format DD:HH:MM:SS
+    # Return:
+    # the value of the date in secondes
+    d, h, m, s = [int(i) for i in date.split(':')]
+    return d*3600*24 + h*3600 + m*60 + s
 
-    def dm_to_dd(self):
-        # convert degrees, minutes, seconds to decimal degrees
-        deg = math.modf(self)[1]
-        min = ((self - deg)*100)
-        dd = float(deg) + float(min)/60
-        return dd
+
+def dm_to_dd(position):
+    # Converts degrees, minutes to decimal degrees
+    # Input:
+    # position: position given in DD.MMMM format
+    # Return:
+    # dd: position converts into decimal degrees
+    deg = math.modf(position)[1]
+    minu = ((position - deg)*100)
+    dd = float(deg) + float(minu)/60
+    return dd
+
+
+def read_scen(filename):
+    # Creates the matrix scenario from a test.ini file
+    # Input:
+    # filename: file test.ini
+    # Return:
+    # scenario: It's a matrix with nb_sections boxes, each of them contain nb_options boxes
+    #           scenario = [[LAT, LONG, ALT, Duration, Heading, Speed, Acceleration, Rate heading,
+    #           Turn rate, turn radius, C/N0, Propagation, antenna, tropospheric model, Ionospheric model,
+    #           Keep altitude, signal type, ...][LAT, LONG, ...]]
+    config = configparser.ConfigParser()
+    config.read(filename)
+    nb_sections = len(config.sections())
+    nb_option = []
+    for i in range(nb_sections):
+        section = config.sections()[i]
+        nb_option.append(len(config.options(section)))
+
+    scenario = [[[] for _ in range(max(nb_option))] for _ in range(nb_sections)]
+    for i in range(nb_sections):
+        section = config.sections()[i]
+        for j in range(nb_option[i]):
+            option = config.options(section)
+            scenario[i][j] = config.get(section, option[j])
+    return scenario
 
 
 def data(filename):
-    # take only the information GGA (global positionning system fix data)
-    # gga = [time in HHMMSS.DD, LAT in DMS, LONG in DMS, ALT in m, N/S, E/W]
+    # Read file and take only the information GGA (global positioning system fix data) data
+    # Input:
+    # filename: file containing NMEA data
+    # Return:
+    # gga: list of GGA data parsing this way:
+    #      gga = [[time in HHMMSS.DD, LAT in DM, LONG in DM, ALT in m, N/S, E/W][...]]
     read = open(filename, 'r')
     gga = []
     a = 1
@@ -32,14 +77,21 @@ def data(filename):
                     a = -1
                 if split[5] == 'W':
                     b = -1
-                gga.append([split[1], a*Tools.dm_to_dd(float(split[2])/100),
-                              b*Tools.dm_to_dd(float(split[4])/100), split[9]])
+                gga.append([split[1], a*dm_to_dd(float(split[2])/100),
+                            b*dm_to_dd(float(split[4])/100), split[9]])
     read.close()
     return gga
 
 
 def heading_compute(lat1, lat2, long1, long2):
     # Compute the heading that has to be followed
+    # Input:
+    # lat1: latitude of the departure position
+    # lat2: latitude of the arrival position
+    # long1: longitude of the departure position
+    # long2: longitude of the arrival position
+    # Return:
+    # heading: heading to be followed from the departure position to reach the arrival one
     angle = abs(math.atan((long1-long2)/(lat1-lat2))*180/math.pi)
     if long2 >= long1:
         if lat2 >= lat1:
